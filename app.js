6b847649531e63ffc270fe31a513aaabe3c1427c
Fchavezb <fchavezb@utn.ac.cr>
2025-06-19 11:05:52 -0600

    cambiar ento

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const port = 8081;
+
+const port = process.env.PORT || 3000;
 
 // Middleware para parsear JSON
 app.use(express.json());
